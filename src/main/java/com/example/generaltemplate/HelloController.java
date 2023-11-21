07f0279beb25976b552dc934a8e841490e442366
David <shu2825@students.d211.org>
2023-11-21 10:56:00 -0600

    Tried making timertask

--- src/main/java/com/example/generaltemplate/HelloController.java
+++ src/main/java/com/example/generaltemplate/HelloController.java
@@ -5,6 +5,13 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
 public class HelloController {
 
     @FXML
@@ -12,13 +19,27 @@ public class HelloController {
 
     @FXML
     public TextField txtInput;
+    private TimerTask task;
+    private int currentTime;
 
     @FXML
-    public void handleClick(ActionEvent actionEvent) {
-        System.out.println("test");
-        lblDisplay.setText("test this");
-        String infoFromTxtField = txtInput.getText();
-        System.out.println(infoFromTxtField);
+    public void startBtnClick(ActionEvent actionEvent) {
+        new Timer().schedule(new EndingGameTask(), 0 , 1000);
+    }
 
+    public class EndingGameTask extends TimerTask {
+        @Override
+        public void run() {
+            System.out.println("Start time is at :" +
+                    LocalDateTime.ofInstant(Instant.ofEpochMilli(scheduledExecutionTime()), ZoneId.systemDefault()));
+            System.out.println("End game in 1 seconds");
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                currentTime++;
+                lblDisplay.setText(String.valueOf(currentTime));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
